91f735402af308e1001337ca87e9633555ce3f82
Soumya Mukherjee <contactsoumyam@gmail.com>
2025-04-11 19:56:53 +0530

    updated layout.tsx for new version toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ServiceWorkerUpdatePrompt from '@/components/ServiceWorkerUpdatePrompt'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
       <body>
         {children}
+        <ServiceWorkerUpdatePrompt />
       </body>
     </html>
   );
